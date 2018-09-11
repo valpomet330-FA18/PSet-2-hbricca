906130fac9a363d24ad34654f0f98ca894bda015
heather bricca <hbricca@KCH-wxcenter-10.valpo.edu>
2018-09-11 17:10:20 -0500

    Added the statements for meteorological spring and summer for record temperatures.

--- climate_check.for
+++ climate_check.for
@@ -20,39 +20,30 @@
 ***************************************************
       
       INTEGER m(11), month, day, Year, monthsum, jd, tmpf, jdsum
- 
+
+*     Open the climate_data.txt file into the program. 
       OPEN (UNIT=15, FILE = 'climate_data.txt', STATUS = 'UNKNOWN')
 
-*     Now read in the file climate_data.txt.
+*     Now read in the file climate_data.txt with a statement to read in the variables.
  
 *      READ (15,*)
       do i=1, 10
         READ (15,*) Year, month, day, tmpf
+*    If statement to see if the year is a leap year or not.
 
-*     Print statement to choose year to then see if it's a leap year.
-
-*      PRINT *, 'Choose a year to see if it is a leap year.'
-*      READ (*,*) Year
-
-*     If the year is divisible by 4, then it's a leap year.
-  
+*     If the year is divisible by 400 or 4, then it's a leap year.
       IF(MOD (Year,400) .EQ. 0 .OR. MOD (Year,4) .EQ. 0 ) THEN
-        
         PRINT *,Year, 'is a leap year. '
         leap = 1
 
 *     If the year is anything else, it is not a leap year.
-
       ELSE 
-
          PRINT *,Year,  'is not a leap year' 
          leap = 0
-
       END IF 
 
 *     This is the else if statement to calculate a Julian day using days
 *     of the month.
-
        sumjd = 0
 
        m(1)= 31
@@ -68,12 +59,10 @@
        m(11)=30
        
        If (MOD (Year,400) .EQ. 0 .AND. MOD (Year,4) .EQ. 0 .AND. month .GT. 2)  THEN
-          
          do j=1, (month-1)
            sumjd= sumjd+m(j)
            enddo 
-           jd= sumjd+day+1
- 
+           jd= sumjd+day+1 
          ELSE
            do j=1, (month-1)
            sumjd= sumjd+ m(j)
@@ -81,12 +70,66 @@
          jd= sumjd+day
          END IF
 
+*     Print statements to print out month, day, year, Julian day, and temperature.
          PRINT *, 'Month=', month
          PRINT *, 'Day=', day
          PRINT *, 'Year=', Year
          PRINT *, 'The number of Julian day is', jd   
          PRINT *, 'Temperature (F)=', tmpf
       
+
+
+*     This starts the part of the script where it indicates Normal temp, Well above average, below average,
+*     and record temperature with meteorological season in which it occured.
+
+*     If statement for records for Winter season.
+      IF (month .EQ. 1 .OR. month .EQ. 2 .OR. month .EQ. 12 .AND. tmpf .EQ. 71) THEN
+         PRINT *, 'For winter, climate records show this temperature is a record high.'
+       ELSE IF (month .EQ. 1 .OR. month .EQ. 2 .OR. month .EQ. 12 .AND. tmpf .EQ. 41) THEN
+        PRINT *, 'For winter, climate records show this temperature is well above average.'
+       ELSE IF (month .EQ. 1 .OR. month .EQ. 2 .OR. month .EQ. 12 .AND. tmpf .EQ. 34.2) THEN
+        PRINT *, 'For winter, climate records show this temperature is average.'
+       ELSE IF (month .EQ. 1 .OR. month .EQ. 2 .OR. month .EQ. 12 .AND. tmpf .EQ. 28) THEN
+        PRINT *, 'For winter, climate records show this temperature is a well below average'
+       ELSE IF (month .EQ. 1 .OR. month .EQ. 2 .OR. month .EQ. 12 .AND. tmpf .EQ. -10) THEN
+        PRINT *, 'For winter, climate records show this temperature is a record low.'
+       
+      END IF
+
+*     If statement for records for Spring season.
+      IF (month .EQ. 3 .OR. month .EQ. 4 .OR. month .EQ. 5 .AND. tmpf .EQ. 96) THEN
+         PRINT *, 'For spring, climate records show this temperature is a record high.'
+       ELSE IF (month .EQ. 3 .OR. month .EQ. 4 .OR. month .EQ. 5 .AND. tmpf .EQ. 71) THEN
+        PRINT *, 'For spring, climate records show this temperature is well above average.'
+       ELSE IF (month .EQ. 3 .OR. month .EQ. 4 .OR. month .EQ. 5 .AND. tmpf .EQ. 58.6) THEN
+        PRINT *, 'For spring, climate records show this temperature is average.'
+       ELSE IF (month .EQ. 3 .OR. month .EQ. 4 .OR. month .EQ. 5 .AND. tmpf .EQ. 47) THEN
+        PRINT *, 'For spring, climate records show this temperature is a well below average'
+       ELSE IF (month .EQ. 3 .OR. month .EQ. 4 .OR. month .EQ. 5 .AND. tmpf .EQ. 13) THEN
+        PRINT *, 'For spring, climate records show this temperature is a record low.' 
+      END IF
       
-      ENDDO
+*     If statement for records for Summer season.
+      IF (month .EQ. 6 .OR. month .EQ. 7 .OR. month .EQ. 8 .AND. tmpf .EQ. 105) THEN
+         PRINT *, 'For summer climate records show this temperature is a record high.'
+       ELSE IF (month .EQ. 6 .OR. month .EQ. 7 .OR. month .EQ. 8 .AND. tmpf .EQ. 87) THEN
+        PRINT *, 'For summer, climate records show this temperature is well above average.'
+       ELSE IF (month .EQ. 6 .OR. month .EQ. 7 .OR. month .EQ. 8 .AND. tmpf .EQ. 81.7) THEN
+        PRINT *, 'For summer, climate records show this temperature is average.'
+       ELSE IF (month .EQ. 6 .OR. month .EQ. 7 .OR. month .EQ. 8 .AND. tmpf .EQ. 77) THEN
+        PRINT *, 'For summer, climate records show this temperature is a well below average'
+       ELSE IF (month .EQ. 6 .OR. month .EQ. 7 .OR. month .EQ. 8 .AND. tmpf .EQ. 52) THEN
+        PRINT *, 'For summer, climate records show this temperature is a record low.' 
+      END IF
+
+
+
+
+
+
+
+
+
+
+      END DO
       END
